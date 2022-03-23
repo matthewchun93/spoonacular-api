a08cba39932be6f13f6cf54323a477b2ec619b0c
matthew <matthewchun93@gmail.com>
2022-03-23 11:22:38 -0700

    checking heroku

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -14,18 +14,18 @@ const Popular = () => {
 
   const fetchData = async () => {
     // localstorage is only used because free api max = 150 calls/day
-    // const check = localStorage.getItem("popular");
-    // if (check) {
-    //   setPopular(JSON.parse(check));
-    // } else {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-    );
-    const data = await api.json();
-    // localStorage.setItem("popular", JSON.stringify(data.recipes));
-    console.log(data);
-    setPopular(data.recipes);
-    // }
+    const check = localStorage.getItem("popular");
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      console.log(data);
+      setPopular(data.recipes);
+    }
   };
 
   return (
